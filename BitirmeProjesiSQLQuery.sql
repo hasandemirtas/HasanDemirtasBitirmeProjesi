c3806b62dc1b8cbdee760ba0927566ddd0720859
Hasan Demirtas <61328406+hasandemirtas@users.noreply.github.com>
2022-08-26 15:18:53 +0300

    sql query

---
+++ BitirmeProjesiSQLQuery.sql
@@ -0,0 +1,11 @@
+insert into Admins values('rea', 'rea@rea.com', 123)
+
+insert into Categories values ('Elektronik')
+insert into Categories values ('Otomotiv')
+insert into Categories values ('Emlak')
+insert into Categories values ('Giyim')
+
+insert into Tag values ('En Çok Satanlar')
+insert into Tag values ('Hýzlý Kargo')
+insert into Tag values ('Kapýda Ödeme')
+insert into Tag values ('2 Yýl Garanti')
